94deb2de21dd153230c4bc1f610f93b7df1d6c36
Jonathan Khoo <jonathan.khoo@csiro.au>
2011-04-14 13:54:41 +1000

    Removed js files.

--- update_plot.js
+++
@@ -1,4 +0,0 @@
-var img = new Image();
-var url = 'http://pulseatparkes.atnf.csiro.au/dev/dfb3_fold_stokes.gif?' + Math.random();
-img.src = url;
-document.getElementById('dfb3_fold_stokes').src = url;
