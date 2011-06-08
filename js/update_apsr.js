c7d7003f32b623ad7ce3148c0ee4759642af62d1
Jonathan Khoo <jonathan.khoo@csiro.au>
2011-06-08 11:52:39 +1000

    Get APSR and CASPSR plots from plots directory.

--- js/update_apsr.js
+++ js/update_apsr.js
@@ -2,16 +2,16 @@ var img = new Image();
 var random = Math.random();
 
 // Pdfb4 fold plot: stokes cyclindrical (pav -SFT)
-var url = 'http://pulseatparkes.atnf.csiro.au/dev/apsr_flux_vs_phase.png?' + random;
+var url = 'http://pulseatparkes.atnf.csiro.au/dev/plots/apsr_flux_vs_phase.png?' + random;
 img.src = url;
 document.getElementById('apsr_flux').src = url;
 
 // Pdfb4 fold plot: time vs phase (pav -YFp)
-url = 'http://pulseatparkes.atnf.csiro.au/dev/apsr_time_vs_phase.png?' + random;
+url = 'http://pulseatparkes.atnf.csiro.au/dev/plots/apsr_time_vs_phase.png?' + random;
 img.src = url;
 document.getElementById('apsr_time').src = url;
 
 // Pdfb4 fold plot: frequency vs time (pav -GTp)
-url = 'http://pulseatparkes.atnf.csiro.au/dev/apsr_freq_vs_phase.png?' + random;
+url = 'http://pulseatparkes.atnf.csiro.au/dev/plots/apsr_freq_vs_phase.png?' + random;
 img.src = url;
 document.getElementById('apsr_freq').src = url;
--- js/update_caspsr.js
+++ js/update_caspsr.js
@@ -2,21 +2,21 @@ var img = new Image();
 var random = Math.random();
 
 // plot: stokes cyclindrical (pav -SFT)
-var url = 'http://pulseatparkes.atnf.csiro.au/dev/caspsr_flux_vs_phase.png?' + random;
+var url = 'http://pulseatparkes.atnf.csiro.au/dev/plots/caspsr_flux_vs_phase.png?' + random;
 img.src = url;
 document.getElementById('caspsr_flux').src = url;
 
 // plot: time vs phase (pav -YFp)
-url = 'http://pulseatparkes.atnf.csiro.au/dev/caspsr_time_vs_phase.png?' + random;
+url = 'http://pulseatparkes.atnf.csiro.au/dev/plots/caspsr_time_vs_phase.png?' + random;
 img.src = url;
 document.getElementById('caspsr_time').src = url;
 
 // plot: frequency vs time (pav -GTp)
-url = 'http://pulseatparkes.atnf.csiro.au/dev/caspsr_freq_vs_phase.png?' + random;
+url = 'http://pulseatparkes.atnf.csiro.au/dev/plots/caspsr_freq_vs_phase.png?' + random;
 img.src = url;
 document.getElementById('caspsr_freq').src = url;
 
 // plot: bandpass (pav -BTp)
-url = 'http://pulseatparkes.atnf.csiro.au/dev/caspsr_bandpass.png?' + random;
+url = 'http://pulseatparkes.atnf.csiro.au/dev/plots/caspsr_bandpass.png?' + random;
 img.src = url;
 document.getElementById('caspsr_bandpass').src = url;
