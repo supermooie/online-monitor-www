6d65ff5aee5c1738bb73473572b4a471aa2774a1
Jonathan Khoo <jonathan.khoo@csiro.au>
2011-06-08 11:51:23 +1000

    Get DFB search-mode plots from plots directory.

--- js/update_dfb3_search.js
+++ js/update_dfb3_search.js
@@ -1,10 +1,10 @@
 var img = new Image();
 var random = Math.random();
 
-var url = 'http://pulseatparkes.atnf.csiro.au/dev/dfb3_search_freq.gif?' + random;
+var url = 'http://pulseatparkes.atnf.csiro.au/dev/plots/dfb3_search_freq.gif?' + random;
 img.src = url;
 document.getElementById('dfb3_search_freq').src = url;
 
-var url = 'http://pulseatparkes.atnf.csiro.au/dev/dfb3_search_hist.gif?' + random;
+var url = 'http://pulseatparkes.atnf.csiro.au/dev/plots/dfb3_search_hist.gif?' + random;
 img.src = url;
 document.getElementById('dfb3_search_hist').src = url;
