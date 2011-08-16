29d15e8bda3209bba7d76e953038f1c683ef29e7
Jonathan Khoo <jonathan.khoo@csiro.au>
2011-08-16 15:50:01 +1000

    Update DFB3 and DFB4 plots from one file.

---
+++ js/update_fold_plots.js
@@ -0,0 +1,40 @@
+var img = new Image();
+var random = Math.random();
+var plot_directory = 'http://pulseatparkes.atnf.csiro.au/dev/plots/';
+var not_observing_image = 'http://pulseatparkes.atnf.csiro.au/dev/images/off.png';
+
+var filenames = new Array(
+  "_fold_stokes.gif",
+  "_fold_time.gif",
+  "_fold_freq.gif",
+  "_fold_bandpass.gif"
+);
+
+var divs = new Array(
+  "_fold_stokes",
+  "_fold_time",
+  "_fold_freq",
+  "_bandpass"
+);
+
+update_plots('dfb3');
+update_plots('dfb4');
+
+function update_plots(backend)
+{
+  var is_observing = 0;
+  // Check if DFBs are creating new plots (i.e. currently observing).
+  $.get('is_observing.php?backend=' + backend, function(data) {
+      is_observing = data;
+  });
+
+  for (var i = 0; i < filenames.length; i++) {
+    var url = is_observing ? plot_directory + backend + filenames[i] + '?' + random : not_observing_image;
+    document.getElementById(backend + divs[i]).src = url;
+  }
+
+  if ($('#' + backend + '_main').is(":visible") == true) {
+    url = plot_directory + $('#' + backend + '_main').data("src").plot + '?' + random;
+    document.getElementById(backend + '_main').src = url;
+  }
+}
